d82dee4f77ac06476aeeedf4490ac6bade18cc07
deana <dvandegriff1@gmail.com>
2022-03-17 09:17:32 -0500

    adding migrations

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,4 @@
 class ActorsController < ApplicationController
-  
   def index
     actors = Actor.all
     render json: actors.as_json
@@ -16,6 +15,8 @@ class ActorsController < ApplicationController
       first_name: params["first_name"],
       last_name: params["last_name"],
       known_for: params["known_for"],
+      gender: params["gender"],
+      age: params["age"],
     )
     actor.save
     render json: actor.as_json
@@ -27,6 +28,8 @@ class ActorsController < ApplicationController
     actor.first_name = params["first_name"] || actor.first_name
     actor.last_name = params["last_name"] || actor.last_name
     actor.known_for = params["known_for"] || actor.known_for
+    actor.gender = params["gender"] || actor.gender
+    actor.age = params["age"] || actor.age
     actor.save
     render json: actor.as_json
   end
@@ -35,14 +38,6 @@ class ActorsController < ApplicationController
     actor_id = params["id"]
     actor = Actor.find_by(id: actor_id)
     actor.destroy
-    render json: {message: "successfully destroyed!"}
+    render json: { message: "successfully destroyed!" }
   end
-
-  
-
-
-
-
-
-
 end
