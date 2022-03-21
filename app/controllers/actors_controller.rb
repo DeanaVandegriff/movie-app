3b05c1bcc237ae216cd65ac0eea34a8e4b716818
deana <dvandegriff1@gmail.com>
2022-03-21 11:01:48 -0500

    hw updates

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,17 +1,17 @@
 class ActorsController < ApplicationController
   def index
-    actors = Actor.all
-    render json: actors.as_json
+    @actors = Actor.all
+    render template: "actors/index"
   end
 
   def show
     actor_id = params["id"]
-    actor = Actor.find_by(id: actor_id)
-    render json: actor.as_json
+    @actor = Actor.find_by(id: actor_id)
+    render template: "actors/show"
   end
 
   def create
-    actor = Actor.new(
+    @actor = Actor.new(
       first_name: params["first_name"],
       last_name: params["last_name"],
       known_for: params["known_for"],
@@ -19,24 +19,27 @@ class ActorsController < ApplicationController
       age: params["age"]
       movie_id: params["movie_id"],
     )
-    if actor.save
+    if @actor.save
       render json: actor.as_json
     else
-      render json: { errors: actor.errors.full_messages }, status: 422
+      render json: { errors: @actor.errors.full_messages }, status: 422
     end
   end
 
   def update
     actor_id = params["id"]
-    actor = Actor.find_by(id: actor_id)
-    actor.first_name = params["first_name"] || actor.first_name
-    actor.last_name = params["last_name"] || actor.last_name
-    actor.known_for = params["known_for"] || actor.known_for
-    actor.gender = params["gender"] || actor.gender
-    actor.age = params["age"] || actor.age
-    actor.movie_id = params["movie_id"] || actor.movie_id
-    actor.save
-    render json: actor.as_json
+    @actor = Actor.find_by(id: actor_id)
+    @actor.first_name = params["first_name"] || @actor.first_name
+    @actor.last_name = params["last_name"] || @actor.last_name
+    @actor.known_for = params["known_for"] || @actor.known_for
+    @actor.gender = params["gender"] || actor.gender
+    @actor.age = params["age"] || actor.age
+    @actor.movie_id = params["movie_id"] || actor.movie_id
+    if @actor.save
+      render json: actor.as_json
+    else
+      render json: { errors: @actor.errors.full_messages }, status: 422
+    end
   end
 
   def destroy
